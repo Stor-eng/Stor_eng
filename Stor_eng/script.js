45b00a6dc99c4426af7ab9260dc6983a1b65d613
‪Mohamed Ahmed‬ <mohamed.elmalek2005@gmail.com>
2026-08-07 22:22:04 +0300

    Update script.js

--- Stor_eng/script.js
+++ Stor_eng/script.js
@@ -1,39 +1,25 @@
 // ===== Firebase =====
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  updateProfile,
-  updateEmail,
-  updatePassword,
-  sendPasswordResetEmail,
-  reauthenticateWithCredential,
-  EmailAuthProvider
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCOBFWl24ZdvuFoS8hwZwiAg3CXsCPPVmQ",
   authDomain: "qotoof-9121a.firebaseapp.com",
   projectId: "qotoof-9121a",
   storageBucket: "qotoof-9121a.firebasestorage.app",
   messagingSenderId: "180620304460",
-  appId: "1:180620304460:web:7fcb7741ca0997685d54e9",
-  measurementId: "G-P8F72XB8J9"
+  appId: "1:180620304460:web:8cf4a43740abcc395d54e9",
+  measurementId: "G-EMPJKRXVVJ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
+const analytics = getAnalytics(app);
 // كاش بسيط لبيانات المستخدم الحالي (بيتحدث من Firebase نفسه، مش من المتصفح)
 let currentUserData = null;
 
